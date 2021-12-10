c0fc7900256e360b2982da074ea8a23569def656
nikhiljain0 <82934775+nikhiljain0@users.noreply.github.com>
2021-12-10 18:21:40 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,10 @@ router.get('/', function(req, res, next) {
 });
 
 /* form handling */
-router.post('/leadform', function (req, res, next) {
+router.post('/leadform', async function (req, res, next) {
   console.log('routing on post form');
   console.log(req.body);
-  const result = pool.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
+  const result = await pool.query(`insert into salesforce.lead(firstname, lastname, doctor__c, mobilephone, postalcode, date_of_birth__c, email, company) Values ('${req.body.txtFirstName}', '${req.body.txtLastName}', '${process.env.DOCTOR_ID}', '${req.body.txtPhoneNumber}', '${req.body.txtZipDC}', '${req.body.txtDOB}', '${req.body.txtEmail}', 'Invisalign')`);
   console.log(result);
   res.render('locator', { success: true });
 });
